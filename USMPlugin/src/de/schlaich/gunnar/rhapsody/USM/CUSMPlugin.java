db9a599514aeabb632d96b539e9faaa26861fc6f
Gunnar <79119288+gschlaich@users.noreply.github.com>
2026-09-16 07:15:05 +0200

    fixed trace

--- USMPlugin/src/de/schlaich/gunnar/rhapsody/USM/CUSMPlugin.java
+++ USMPlugin/src/de/schlaich/gunnar/rhapsody/USM/CUSMPlugin.java
@@ -1400,7 +1400,7 @@ public class CUSMPlugin extends RPUserPlugin
 		{
 			if(selected instanceof IRPHyperLink == false)
 			{
-				trace("No XML File");
+				trace(selected.getName()+": No XML File");
 				return;
 			}	
 			
